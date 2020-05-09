dc438543c973a7ca0db96b0bb8700ab32c3e47a6
ElectroScience <46717842+ElectroSciences@users.noreply.github.com>
2020-05-09 17:00:19 +0200

    Add files via upload

---
+++ +_-_V2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+    
+    const int MAX = 100, MIN = 0;
+    int tn = 0;
+    int ns = 0;
+    srand(time(NULL));
+    int mn = (rand() % (MAX - MIN + 1)) + MIN;
+    
+    printf("Let's go !\n");
+    
+    while(tn != mn){
+        printf("What number ?");
+        scanf("%d", &tn);
+        if (tn < mn)
+            printf("Taller !\n");
+        if (tn > mn)
+            printf("Smaller !\n");
+    
+        ns ++;
+    }
+    
+    if (tn == mn)
+        printf("\nCongratulations, you won in %d move(s) !      \n\n", ns);
+    
+    return 0;
+    
+}
+
