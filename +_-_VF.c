c830d8449c0b4a4877aa040c7550519eaf31f0ba
ElectroScience <46717842+ElectroSciences@users.noreply.github.com>
2020-05-13 22:25:51 +0200

    Add files via upload

---
+++ +_-_VF.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+    
+    const int MAX = 100, MIN = 0;
+    char YN = 'A';
+    int tn = 0;
+    int ns = 0;
+    srand(time(NULL));
+    int mn = (rand() % (MAX - MIN + 1)) + MIN;
+    
+    printf(" Hello, my name is computplay, we're gonna play a game, all right ? (Y/N)");
+    scanf("%c", &YN);
+    if(YN == 'Y'){
+        printf(" Great, so the game is called + or -, I focus on a number, and you have to guess it, ready ? (Y/N)");
+        scanf(" %c", &YN);
+        
+        if(YN == 'Y'){
+            printf("Let's go !\n");
+            
+            while(tn != mn){
+                printf("What number ?");
+                scanf(" %d", &tn);
+                if (tn < mn)
+                    printf("Bigger !\n");
+                if (tn > mn)
+                    printf("Smaller !\n");
+                ns ++;
+            }
+            if (tn == mn)
+                printf("\nCongratulations, you won in %d move(s) !\n\n", ns);
+        }
+        
+        if(YN == 'N')
+            printf(" Well, well, goodbye then :'( \n\n");
+        
+    }
+    
+    
+    else
+        printf(" Well, well, goodbye then :'( \n\n");
+    
+    
+    
+    
+}
